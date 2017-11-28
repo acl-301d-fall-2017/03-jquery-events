78ba69107d1ce5eba6da3f0bda62b3ed73299f81
CharlyWelch <blwbiology@gmail.com>
2017-11-27 21:40:02 -0800

    fix category selector, add read on links

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -29,13 +29,11 @@ articleView.populateFilters = function() {
 articleView.handleAuthorFilter = function() {
     $('#author-filter').on('change', function() {
         if ($(this).val()) {
-            $('#articles').hide();
-            $(`#articles[name=${this.author}]`).show();
-            // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
-            // Use an "attribute selector" to find those articles, and fade them in for the reader.
-
+            $('article').hide();
+            $(`article[data-author = '${$(this).val()}']`).fadeIn();
         } else {
-            $('#articles.draft').show();
+            $('article').show();
+            $('article.template').hide();
         }
         $('#category-filter').val('');
     });
@@ -44,35 +42,34 @@ articleView.handleAuthorFilter = function() {
 articleView.handleCategoryFilter = function() {
     $('#category-filter').on('change', function() {
         if ($(this).val()) {
-            $('#articles').hide();
-            $(`#articles[category=${this.category}]`).show(); //does not work
-
+            $('article').hide();
+            $(`article[data-category = '${$(this).val()}']`).fadeIn();
         } else {
-            $('#articles.draft').show();
+            $('article').show();
+            $('article.template').hide();
         }
         $('#author-filter').val('');
     });
-
-    // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
-    // When an option with a value is selected, hide all the articles, then reveal the matches.
-    // When the blank (default) option is selected, show all the articles, except for the template.
-    // Be sure to reset the #author-filter while you are at it!
-
 };
 
 articleView.handleMainNav = function() {
+    $('li[class = tab]').click(changeTabs);
+    function changeTabs(){
+        const clickedTab = $(this).attr('data-content');
+        console.log(clickedTab + 'was Clicked');
+        $('.tab-content').hide();
+        $(`section#${clickedTab}`).show();
+    }
+    
     // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
     // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
     // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
-    const clickedTabData = $(this).attr('data-content');//not working
-    console.log(`${clickedTabData} was clicked!`);//not working
-    // $('.tab-content').hide();
-    // $(`section#${clickedTabData}`).show();
     
+
     // REVIEW: Now trigger a click on the first .tab element, to set up the page.
+   
     $('.main-nav .tab:first').click();
 };
-$('.tab').click(articleView.handleMainNav());
 
 articleView.setTeasers = function() {
     // REVIEW: Hide elements beyond the first 2 in any article body.
@@ -80,6 +77,12 @@ articleView.setTeasers = function() {
 
     // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
     // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+    $('a[class = read-on]').click(readOn);
+    function readOn(){
+        event.preventDefault();
+
+        $(this).parent().find('.article-body *').show();
+    }
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
@@ -88,4 +91,6 @@ $(document).ready(function() {
     articleView.populateFilters();
     articleView.handleAuthorFilter();
     articleView.handleCategoryFilter();
+    articleView.handleMainNav();
+    articleView.setTeasers();
 });
