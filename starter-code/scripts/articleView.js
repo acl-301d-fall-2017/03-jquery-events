0efa8b65dba9cc9e3b9d88f5bc4e98af0172c226
Charlie Heiner <heinercj@gmail.com>
2017-11-27 17:25:57 -0800

    added tab feature to main nav

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -83,7 +83,13 @@ articleView.handleMainNav = function() {
     // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
     // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
     // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
-
+    $('li[class = tab]').click(changeTabs);
+    function changeTabs(){
+        const clickedTab = $(this).attr('data-content');
+        console.log(clickedTab + 'was Clicked');
+        $('.tab-content').hide();
+        $(`section#${clickedTab}`).show();
+    }
     // REVIEW: Now trigger a click on the first .tab element, to set up the page.
     $('.main-nav .tab:first').click();
 };
